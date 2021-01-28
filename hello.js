11406deaa7f41d8bc78f3912023e1af49b4f3930
SMMainulHasan <mainulhasan7070@gmail.com>
2021-01-28 16:58:01 +0600

    hfhg

--- hello.js
+++ hello.js
@@ -3,12 +3,20 @@ const plusBtn = document.getElementById("plusiphone");
 plusBtn.addEventListener("click", function () {
     const iphoneQuentity = document.getElementById("quentityiphone").value;
     const iphoneQuentityNum = parseFloat(iphoneQuentity);
-    document.getElementById("quentityiphone").value = iphoneQuentityNum + 1;
+    const Quentity = document.getElementById("quentityiphone").value = iphoneQuentityNum + 1;
+    // const iphonePrice = document.getElementById("iphonePrice").innerText;
+    // const iphonePriceNum = parseFloat(iphonePrice);
+    const price = 1219 * Quentity;
+    document.getElementById("iphonePrice").innerText = price;
 })
 //minus button
 const minusBtn = document.getElementById("minusiphone");
 minusBtn.addEventListener("click", function () {
     const iphoneQuentity = document.getElementById("quentityiphone").value;
     const iphoneQuentityNum = parseFloat(iphoneQuentity);
-    document.getElementById("quentityiphone").value = iphoneQuentityNum - 1;
+    const quentity = document.getElementById("quentityiphone").value = iphoneQuentityNum - 1;
+    // const iphonePrice = document.getElementById("iphonePrice").innerText;
+    // const iphonePriceNum = parseFloat(iphonePrice);
+    const price = 1219 * quentity;
+    document.getElementById("iphonePrice").innerText = price;
 })
