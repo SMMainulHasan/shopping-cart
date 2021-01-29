58e3d2c48747ec44dfe41282e1f2c2e71f02153a
SMMainulHasan <mainulhasan7070@gmail.com>
2021-01-29 12:47:34 +0600

    jdfhg

--- hello.js
+++ hello.js
@@ -1,22 +1,15 @@
-//plus button
-const plusBtn = document.getElementById("plusiphone");
-plusBtn.addEventListener("click", function () {
-    const iphoneQuentity = document.getElementById("quentityiphone").value;
-    const iphoneQuentityNum = parseFloat(iphoneQuentity);
-    const Quentity = document.getElementById("quentityiphone").value = iphoneQuentityNum + 1;
-    // const iphonePrice = document.getElementById("iphonePrice").innerText;
-    // const iphonePriceNum = parseFloat(iphonePrice);
-    const price = 1219 * Quentity;
-    document.getElementById("iphonePrice").innerText = price;
-})
-//minus button
-const minusBtn = document.getElementById("minusiphone");
-minusBtn.addEventListener("click", function () {
-    const iphoneQuentity = document.getElementById("quentityiphone").value;
-    const iphoneQuentityNum = parseFloat(iphoneQuentity);
-    const quentity = document.getElementById("quentityiphone").value = iphoneQuentityNum - 1;
-    // const iphonePrice = document.getElementById("iphonePrice").innerText;
-    // const iphonePriceNum = parseFloat(iphonePrice);
-    const price = 1219 * quentity;
-    document.getElementById("iphonePrice").innerText = price;
-})
+
+function addQuantity(inCrease) {
+    const iphoneQuantity = document.getElementById("Quantityiphone");
+    const iphoneQuantityNum = parseInt(iphoneQuantity.value);
+    let Quantity = iphoneQuantityNum;
+    if (inCrease == true) {
+        Quantity = iphoneQuantityNum + 1;
+    }
+    if (inCrease == false && Quantity > 1) {
+        Quantity = iphoneQuantityNum - 1;
+    }
+    iphoneQuantity.value = Quantity;
+    const price = 1219 * Quantity;
+    document.getElementById("iphonePrice").innerText = "$" + price;
+}
